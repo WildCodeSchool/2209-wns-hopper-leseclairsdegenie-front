b7872cdb378b2af0156fbe4287e8aefca6cceb48
TELLAI <tellaiyt@gmail.com>
2023-01-26 10:48:16 +0100

    initialisation de la page HOME

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  useMutation,
-} from "@apollo/client";
-import { createUser } from "./graphql/createUser";
-
-function Signup() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const [doSignupMutation, { data, loading, error }] = useMutation(createUser);
-
-  async function doSignup() {
-    try {
-      await doSignupMutation({
-        variables: {
-          data: {
-            email,
-            password,
-          },
-        },
-      });
-      setEmail("");
-      setPassword("");
-    } catch {}
-  }
-
-  return (
-    <>
-      {error && (
-        <pre style={{ color: "red" }}>{JSON.stringify(error, null, 4)}</pre>
-      )}
-      <p>Email:</p>
-      <input
-        disabled={loading}
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <p>Password:</p>
-      <input
-        disabled={loading}
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button disabled={loading} onClick={doSignup}>
-        Signup
-      </button>
-    </>
-  );
-}
-
-function Main() {
-  return (
-    <div>
-      <Signup />
-    </div>
-  );
-}
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import Home from "./pages/Home";
+//import BandeauHome from "./components/BandeauHome";
 
 const client = new ApolloClient({
   uri: "http://localhost:5000",
@@ -72,7 +12,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Main />
+      <Home />
     </ApolloProvider>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BandeauHome from "../components/BandeauHome";
+import CategoryHome from "../components/CategoryHome";
+
+const Home = () => {
+  return (
+    <div>
+      <BandeauHome />
+      <CategoryHome />
+    </div>
+  );
+};
+
+export default Home;
