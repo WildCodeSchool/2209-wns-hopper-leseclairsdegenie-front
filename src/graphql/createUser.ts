180936c0492b0cb276cd443ac2a3387ac2145caa
Oscar-AGUDELO <oscar.agudelo.pro@gmail.com>
2023-01-26 11:47:14 +0100

    fixDockerWin and signup

--- src/graphql/createUser.ts
+++ src/graphql/createUser.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const createUser = gql`
-  mutation CreateUser($data: UserInput!) {
+  mutation Mutation($data: UserInput!) {
     createUser(data: $data) {
       id
     }
