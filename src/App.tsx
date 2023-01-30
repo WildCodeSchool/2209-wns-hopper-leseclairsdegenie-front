3a57f6589dbcc60cea2508f49bc63ca414049c98
jp-justine <justine.petit@aol.com>
2023-01-30 09:40:21 +0100

    finished nav footer their css and the position relative to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,30 @@
 import React from "react";
 import "./App.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
 import Signup from "./components/Signup";
+import Navbar from "./components/nav/Navbar";
+import Footer from "./components/footer/Footer";
 
 function Main() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            {/* <li>
-              <Link to="/">Home</Link>
-            </li> */}
-            <li>
-              <Link to="products">Produits</Link>
-            </li>
-            {/* <li>
-              <Link to="reservations">Réservations</Link>
-            </li> */}
-            {/* <li>
-              <Link to="cart">Panier</Link>
-            </li> */}
-            <li>
-              <Link to="signup">Connexion</Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar />
 
         {/* A <Routes> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
           <Route path="/categorie/:name" element={<Category />} />
-          {/* <Route path="reservations" element={<Reservations />} /> */}
-          {/* <Route path="cart" element={<Cart />} /> */}
+          {/* <Route path="reservations" element={<Reservations />} />
+          <Route path="cart" element={<Cart />} /> */}
           <Route path="signup" element={<Signup />} />
         </Routes>
       </div>
+      <Footer />
     </Router>
   );
 }
@@ -54,7 +38,6 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Main />
-      <Home />
     </ApolloProvider>
   );
 }
