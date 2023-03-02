c40e86b48a92e57efd16784df7c781fcccd5ec36
JUSTINE GETS <[justine.gets@posteo.net](mailto:justine.gets@posteo.net)>
2023-03-02 14:25:37 +0100

    Save

--- src/components/products/ProductDetails.tsx
+++ src/components/products/ProductDetails.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from "@apollo/client";
-import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useParams } from "react-router-dom";
 import { getProduct } from "../../graphql/productQueries";
 import { IProduct } from "../../interfaces";
+import { MainContext } from "../../MainContexts";
 
 export default function ProductDetails () {
 
@@ -9,6 +11,8 @@ export default function ProductDetails () {
     const { loading, data, refetch } = useQuery<{ product: IProduct }>(
         getProduct, {variables : { "id": `${productId}`}}
       );
+
+    const Main = useContext(MainContext);
 
     if (loading) return (<div>Loading...</div>);
     const product = data ? data.product : null;
@@ -20,15 +24,22 @@ export default function ProductDetails () {
                 <h2>{product?.name}</h2>
                 <p> {product?.description} </p>
                 <label>du : <span> </span> 
-                <input type="date" id="start"
-                        value="2023-02-03"
-                        min="2023-02-03" max="2024-02-03"/>
+                    <input type="date" id="start"
+                            value="2023-02-03"
+                            min="2023-02-03" max="2024-02-03"/>
                 </label>
                 <label>au : <span></span> 
-                <input type="date" id="end"
-                        value="2023-02-03"
-                        min="2023-02-03" max="2024-02-03"/>
-            </label>     
+                    <input type="date" id="end"
+                            value="2023-02-03"
+                            min="2023-02-03" max="2024-02-03"/>
+                </label>
+                <div>
+                    {Main?.user?.id ? (
+                        <Link className="product-details-link" to={`/products/product-details/${product?.id}`}>+ Détails</Link> 
+                    ) : (
+                        <Link to="../../../connection">Connexion</Link>
+                    )}
+                </div>     
             </div>
         </div>
     )
