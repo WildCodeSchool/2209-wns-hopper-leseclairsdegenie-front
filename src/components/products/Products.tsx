22a0fd373ebd5f36fc3ecc6e64c141141809b2dd
jp-justine <justine.petit@aol.com>
2023-05-25 10:15:23 +0200

    finished filter by categories

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,18 +1,20 @@
 import { useQuery } from "@apollo/client";
 import { getProducts } from "../../graphql/productQueries";
-import { IProduct } from "../../interfaces";
+import { ICategory, IProduct } from "../../interfaces";
 import ProductCard from "./ProductCard";
 import "./Products.css";
 import { useState } from "react";
+import { getCategories } from "../../graphql/Category";
 
 export default function Products() {
-  const { loading, data } = useQuery<{ products: IProduct[] }>(
-    getProducts
+  const { loading, data } = useQuery<{ products: IProduct[] }>(getProducts);
+  const { loading:categoriesLoading, data: categories } = useQuery<{ categories: ICategory[] }>(
+    getCategories
   );
+  console.log(categories);
 
-  const [filteredList, setFilteredList] = useState< IProduct[]>();
-
-  const products = data ? data.products : null;
+  const [filteredList, setFilteredList] = useState<IProduct[]>();
+  const [selectedCategory, setSelectedCategory] = useState();
 
   const search = (event: any) => {
     const query = event.target.value;
@@ -25,7 +27,6 @@ export default function Products() {
       );
     });
     setFilteredList(updatedList);
-    console.log(filteredList);
   };
 
   //autre possibilite de recherche mais avec typescript on est trop limité
@@ -37,7 +38,20 @@ export default function Products() {
   //   );
   // };
 
+  function handleCategoryChange(event: any) {
+    setSelectedCategory(event.target.value);
+    if (!event.target.value) {
+      return products;
+    }
+    const filteredList = products?.filter((item) => item.category.name === event.target.value);
+    setFilteredList(filteredList);
+  }
+
   if (loading) return <div>Loading...</div>;
+  if (categoriesLoading) return <div>Loading...</div>;
+
+  const products = data ? data.products : null;
+  const category = categories ? categories.categories : null;
 
   return (
     <div className="products-page">
@@ -48,26 +62,40 @@ export default function Products() {
               type="text"
               placeholder="recherche..."
               className="search"
-              // onChange={(e) => setQuery(e.target.value.toLowerCase())}
               onChange={search}
             />
+          </li>
+          <li>
+            <select
+              name="category-list"
+              id="category-list"
+              onChange={handleCategoryChange}
+            >
+              <option>choose option</option>
+              {category?.map(
+                (item) => (
+                  // console.log(item),
+                  (<option key={item.id}>{item.name}</option>)
+                )
+              )}
+              ;
+            </select>
           </li>
         </ul>
       </header>
       <main>
         <section className="card-row">
-          {filteredList ? filteredList.map((item)=>(
-           <ProductCard item={item} />
-            )): 
+          {filteredList ? (
+            filteredList.map((item) => (
+              <ProductCard key={item.id} item={item} />
+            ))
+          ) : (
             <section className="card-row">
-            {products?.map((product) => {
-              return (
-                <ProductCard item={product}/>
-              );
-            })}
-          </section>
-
-          }
+              {products?.map((product) => {
+                return <ProductCard key={product.id} item={product} />;
+              })}
+            </section>
+          )}
         </section>
       </main>
     </div>
