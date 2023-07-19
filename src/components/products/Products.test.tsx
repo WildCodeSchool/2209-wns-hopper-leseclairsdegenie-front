18c43b41be592c8bbb7a154e1e1e1e2bc30f810f
jp-justine <justine.petit@aol.com>
2023-07-19 09:25:33 +0200

    solved test

--- src/components/products/Products.test.tsx
+++ src/components/products/Products.test.tsx
@@ -1,37 +1,31 @@
-// import { render, screen } from "@testing-library/react";
-// // import userEvent from '@testing-library/user-event'
-// import "@testing-library/jest-dom";
-// import ProductCard from "./ProductCard";
+import { render, screen } from "@testing-library/react";
+// import userEvent from '@testing-library/user-event'
+import "@testing-library/jest-dom";
+import ProductCard from "./ProductCard";
 
-// test("loads and displays price's product", async () => {
-//   // ARRANGE
-//   render(
-//     <ProductCard
-//       item ={
-//         {
-//           description: "Chaussures de randonnée",
-//           disponibility: true,
-//           image: "",
-//           name: "Chaussures de randonnée",
-//           price: 42.25,
-//           quantity:1,
-//            id:1, 
-//            category: {
-//             name:"",
-//             id: 1
-//            } ,
-//         }
-//       }
-//     />
-//   );
+test("loads and displays price's product", async () => {
+  // ARRANGE
+  render(
+    <ProductCard
+      item ={
+        {
+          description: "Chaussures de randonnée",
+          image: "",
+          name: "Chaussures de randonnée",
+          price: 42.25,
+           id:1, 
+        }
+      }
+    />
+  );
 
-//   // ACT
-//   //   await userEvent.click(screen.getByText('Load Greeting'))
-//   //   await screen.findByRole('heading')
+  // ACT
+  //   await userEvent.click(screen.getByText('Load Greeting'))
+  //   await screen.findByRole('heading')
 
-//   // ASSERT
-//   expect(screen.getByTestId("product-price")).toHaveTextContent(
-//     "42.25€ / jour"
-//   );
-//   //   expect(screen.getByRole('button')).toBeDisabled()
-// });
+  // ASSERT
+  expect(screen.getByTestId("product-price")).toHaveTextContent(
+    "42.25€ / jour"
+  );
+  //   expect(screen.getByRole('button')).toBeDisabled()
+});
